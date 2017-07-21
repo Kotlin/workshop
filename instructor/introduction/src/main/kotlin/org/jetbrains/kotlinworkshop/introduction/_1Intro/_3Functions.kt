a473e4d56c6bedceb4b72a4c4cb50d78817bddfe
hadihariri <mail@hadihariri.com>
2017-07-21 15:39:45 +0200

    Added vararg and spread examples

--- instructor/introduction/src/main/kotlin/org/jetbrains/kotlinworkshop/introduction/_1Intro/_3Functions.kt
+++ instructor/introduction/src/main/kotlin/org/jetbrains/kotlinworkshop/introduction/_1Intro/_3Functions.kt
@@ -9,11 +9,30 @@ fun max2(a: Int, b: Int) = if (a < b) a else b
 // default values for arguments
 fun sum(a: Int, b: Int, c: Int = 0) = a + b + c
 
-fun use() {
+
+// vararg
+fun printMany(vararg elements: Int) {
+    for (element in elements) {
+        println(element)
+    }
+
+    doSomething(*elements)
+
+}
+
+fun doSomething(vararg elements: Int) {
+    for (element in elements) {
+        println("Doing something $element")
+    }
+}
+
+fun main(args: Array<String>) {
     sum(1, 2)
     sum(1, 2, 3)
     // named arguments
     sum(b = 2, a = 1)
+
+    printMany(1, 2, 3, 4, 5)
 }
 
 
