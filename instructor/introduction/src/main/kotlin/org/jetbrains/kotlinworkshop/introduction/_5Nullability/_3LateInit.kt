19e0e7f18a9f9294ec020156b69912eebb88e804
hadihariri <mail@hadihariri.com>
2017-07-21 15:36:21 +0200

    Added example of lateinit

---
+++ instructor/introduction/src/main/kotlin/org/jetbrains/kotlinworkshop/introduction/_5Nullability/_3LateInit.kt
@@ -0,0 +1,12 @@
+package org.jetbrains.kotlinworkshop.introduction._5Nullability
+
+
+class Dependency
+class Consumer {
+    lateinit var dependency: Dependency
+
+    fun usingDependency() {
+
+    }
+
+}
