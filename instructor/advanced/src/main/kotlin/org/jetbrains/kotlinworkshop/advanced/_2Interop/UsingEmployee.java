31f98bdd586125e5d42cd1a23c4de705d2df7024
hadihariri <mail@hadihariri.com>
2017-07-21 15:31:56 +0200

    Added Examples for annotations

---
+++ instructor/advanced/src/main/kotlin/org/jetbrains/kotlinworkshop/advanced/_2Interop/UsingEmployee.java
@@ -0,0 +1,15 @@
+package org.jetbrains.kotlinworkshop.advanced._2Interop;
+
+public class UsingEmployee {
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee();
+
+        employee.getField();
+        employee.setField("Value");
+
+
+
+    }
+}
--- instructor/advanced/src/main/kotlin/org/jetbrains/kotlinworkshop/advanced/_2Interop/_3InteropAnnotations.kt
+++ instructor/advanced/src/main/kotlin/org/jetbrains/kotlinworkshop/advanced/_2Interop/_3InteropAnnotations.kt
@@ -1,3 +1,19 @@
+@file:JvmName("Foo")
+
 package org.jetbrains.kotlinworkshop.advanced._2Interop
 
 
+
+class Employee {
+    // @JvmField will make this a field and eliminate property accessors
+    var field = ""
+}
+
+fun process(input: List<Int>) {
+
+}
+
+@JvmName("processStrings")
+fun process(input: List<String>) {
+
+}
