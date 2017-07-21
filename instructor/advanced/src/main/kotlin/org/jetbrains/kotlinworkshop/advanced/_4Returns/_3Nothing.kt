90ae62b83e4c36c74521e02618522e156edd3415
hadihariri <mail@hadihariri.com>
2017-07-21 15:17:06 +0200

    Example of Nothing

--- instructor/advanced/src/main/kotlin/org/jetbrains/kotlinworkshop/advanced/_4Returns/_3Nothing.kt
+++ instructor/advanced/src/main/kotlin/org/jetbrains/kotlinworkshop/advanced/_4Returns/_3Nothing.kt
@@ -1,3 +1,18 @@
 package org.jetbrains.kotlinworkshop.advanced._4Returns
 
+// Add Nothing
+fun endlessLoop() {
 
+    while (true) {
+        // do something
+    }
+
+}
+
+
+fun main(args: Array<String>) {
+
+
+    endlessLoop()
+    println("Never will reach here")
+}
