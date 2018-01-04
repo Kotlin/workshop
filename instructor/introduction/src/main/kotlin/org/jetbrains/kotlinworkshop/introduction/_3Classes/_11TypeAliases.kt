c2f88d883581ea8959c9a3ebfd1319cce3830338
Raphael R <reitzig@users.noreply.github.com>
2018-01-04 14:34:54 +0100

    Deprecation annotation was on the wrong symbol
    The compiler would warn
    
    > 'typealias BasicCustomer = BaseCustomer' uses 'BaseCustomer', which is deprecated. BasicCustomer is now called BaseCustomer
    
    But `BaseCustomer` is not deprecated. After the change, the warning is:
    
    > 'typealias BasicCustomer = BaseCustomer' is deprecated. BasicCustomer is now called BaseCustomer

--- instructor/introduction/src/main/kotlin/org/jetbrains/kotlinworkshop/introduction/_3Classes/_11TypeAliases.kt
+++ instructor/introduction/src/main/kotlin/org/jetbrains/kotlinworkshop/introduction/_3Classes/_11TypeAliases.kt
@@ -7,9 +7,9 @@ typealias CustomerName = String
 // Example of using typealias for rename with @Deprecated
 
 
-@Deprecated("BasicCustomer is now called BaseCustomer", replaceWith = ReplaceWith("BaseCustomer"))
 class BaseCustomer // before was called BasicCustomer
 
+@Deprecated("BasicCustomer is now called BaseCustomer", replaceWith = ReplaceWith("BaseCustomer"))
 typealias BasicCustomer = BaseCustomer
 
 
